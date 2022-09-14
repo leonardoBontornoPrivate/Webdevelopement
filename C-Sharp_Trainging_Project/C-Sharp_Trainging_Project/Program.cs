6178ecd765ff9e9c12457adc8fc4c632ff7d3232
PersonalProfile <113506465+leonardoBontornoPrivate@users.noreply.github.com>
2022-09-15 00:46:30 +0200

    Add files via upload

---
+++ C-Sharp_Trainging_Project/C-Sharp_Trainging_Project/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace C_Sharp_Trainging_Project
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Arrays and Lists";
+
+            Console.Write("How many students do you want to have? ");
+            int studentsCount = Convert.ToInt32(Console.ReadLine());
+
+            string[] names = new string[studentsCount];
+
+            for(int i = 0; i < names.Length; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("-------------------------------------");
+
+            Array.Sort(names);
+
+            foreach(string _names in names)
+            {
+                Console.WriteLine(_names);
+            }
+
+            /* 
+             * while
+             * do-while
+             * for
+             * foreach
+             */
+
+            // Wait for Key to end the program
+            Console.ReadKey();
+        }
+    }
+}
